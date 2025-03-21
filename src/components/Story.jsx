1fec9786fb13df156bfe318a8ccd2283ec1dcc76
akila <akilaocj@gmail.com>
2025-03-21 18:01:17 +1100

    Update team image in Story component to new format

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,5 +1,5 @@
 import Reveal from "./animations/Reveal";
-import teamImage from "../assets/our-story.JPG";
+import teamImage from "../assets/our-story3.png";
 
 export default function Story() {
   return (
